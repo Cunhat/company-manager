6f3ff16aa0a2b7023357f466d084e153ba695e92
Tiago Cunha <tiago92cunha@gmail.com>
2026-09-07 20:47:54 +0100

    Add kms paths table

--- packages/db/src/schema/index.ts
+++ packages/db/src/schema/index.ts
@@ -1,5 +1,6 @@
 export * from "./auth";
 export * from "./invoice";
 export * from "./expense";
+export * from "./kms_path";
 
 export {};
---
+++ packages/db/src/schema/kms_path.ts
@@ -0,0 +1,25 @@
+import { integer, pgTable, uuid, text, timestamp } from "drizzle-orm/pg-core";
+import { user } from "./auth";
+import { relations } from "drizzle-orm";
+
+export const kmsPath = pgTable("kms_path", {
+  id: uuid("id").primaryKey(),
+  origin: text("origin").notNull(),
+  destination: text("destination").notNull(),
+  reason: text("reason").notNull(),
+  distance: integer("distance").notNull(),
+  date: timestamp("date").notNull(),
+  description: text("description"),
+  updatedAt: timestamp("updated_at").defaultNow().notNull(),
+
+  userId: text("user_id")
+    .notNull()
+    .references(() => user.id, { onDelete: "cascade" }),
+});
+
+export const kmsPathRelations = relations(kmsPath, ({ one }) => ({
+  user: one(user, {
+    fields: [kmsPath.userId],
+    references: [user.id],
+  }),
+}));
